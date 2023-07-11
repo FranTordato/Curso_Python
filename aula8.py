0c6cf0aa953ef18b2c7a4cb41fc683d8e6528082
Fran Tordato <106915881+FranTordato@users.noreply.github.com>
2023-07-11 07:54:45 -0300

    Add files via upload

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome='Fran'
+sobrenome='Tordato'
+idade=42
+ano_nascimento=2023-idade
+maior_de_idade= idade>=18
+altura_metros=1.56
+print('Nome =',nome)
+print('Sobrenome =',sobrenome)
+print('Idade =',idade)
+print('Ano de nascimento =',ano_nascimento)
+print('Maior de idade ?',maior_de_idade)
+print('Altura =',altura_metros)
