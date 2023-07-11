ccb7fa5ae51c74fbc36d82516703855dcfa860ea
Fran Tordato <106915881+FranTordato@users.noreply.github.com>
2023-07-11 07:56:02 -0300

    Add files via upload

---
+++ exercicio_aula19.py
@@ -0,0 +1,8 @@
+primeiro_valor=input('digite um valor: ')
+segundo_valor= input('digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print ( f'primeiro_valor = {primeiro_valor}  é maior ou igual ao segundo_valor = {segundo_valor}')
+
+else:
+    print ( f'segundo_valor = {segundo_valor} é maior do que primeiro_valor = {primeiro_valor}')
